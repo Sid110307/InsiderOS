ee7218d61ecb23bfe7c3ba126a10c22361c2cd74
Sid110307 <srsfool@gmail.com>
2023-04-30 12:21:45 +0530

    Update

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-if [ "$1" = "-s" ] || [ "$1" = "--silent" ]; then
-	cd gnu-efi
-	make bootloader -s
-	cd ../kernel/
-	make clean -s
-	make kernel -s
-	make buildimg -s
-	make run -s
-	cd ../
-else
-	cd gnu-efi
-	make bootloader
-	cd ../kernel/
-	make clean -s
-	make kernel
-	make buildimg
-	make run
-	cd ../
-fi
+OS_IMG=bin/InsiderOS
+
+if [ "$1" = "-s" ] || [ "$1" = "--silent" ]; then SILENT=-s; fi
+
+cd gnu-efi || exit 2
+make bootloader $SILENT
+
+cd ../kernel || exit 2
+make clean $SILENT
+make kernel $SILENT
+make buildimg $SILENT
+sudo rm -f $OS_IMG.vdi
+VBoxManage convertfromraw --format VDI $OS_IMG.img $OS_IMG.vdi
+make run $SILENT
+cd ..
--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,7 +1,5 @@
 #include "include/kernelUtils.h"
 
-// uint8_t testBuffer[20];
-
 extern "C" void _start(BootInfo * bootInfo)
 {
 	GDTDescriptor gdtDescriptor;
@@ -13,6 +11,10 @@ extern "C" void _start(BootInfo * bootInfo)
 	KernelInfo kernelInfo = InitializeKernel(bootInfo);
 	PageTableManager* pageTableManager = kernelInfo.pageTableManager;
 
+	GlobalRenderer->ClearColor = COLOR_CYAN;
+	GlobalRenderer->Clear();
+
+	GlobalRenderer->Color = COLOR_RED;
 	GlobalRenderer->Print("Kernel Initialized Successfully!");
 
 	while (true);
